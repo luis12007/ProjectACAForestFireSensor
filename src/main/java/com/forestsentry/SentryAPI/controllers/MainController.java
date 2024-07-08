b8c2af26a6f19906bce33dcf4588e4496a7e6732
Gabriel Martinez <gabriel.emv411@gmail.com>
2024-07-07 19:40:03 -0600

    Fix 6

--- src/main/java/com/forestsentry/SentryAPI/controllers/MainController.java
+++ src/main/java/com/forestsentry/SentryAPI/controllers/MainController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.forestsentry.SentryAPI.models.dtos.RecordDTO;
@@ -35,7 +38,8 @@ public class MainController {
     @Autowired
     private RecordService recordService;
 
-    @PostMapping("/Post")
+    @RequestMapping(value = "/greeting", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public ResponseEntity<?> saveRecord(@RequestBody RecordDTO dto){
         try{
             System.out.println("Entro a la mierda ---------------------");
